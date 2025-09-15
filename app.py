38dac9f5ccac8277f0b38c1eee7cd63963585b8c
tcarmo2023 <148493266+tcarmo2023@users.noreply.github.com>
2025-09-15 18:18:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ WHATSAPP_NUMBER = "558185641262"
 ADMIN_PASSWORD = "CODE@2025"
 
 # URLs das imagens
-BORBOLETA_URL = "/static/images/borboleta.png"
-QR_URL = "/static/images/qrcode-pix.svg"
-LOGO_URL = "/static/images/logo.jpeg"
-QUEM_SOMOS_LOGO = "/static/images/Quem somos.png"
+BORBOLETA_URL = "/borboleta.png"
+QR_URL = "/qrcode-pix.svg"
+LOGO_URL = "/logo.jpeg"
+QUEM_SOMOS_LOGO = "/Quem somos.png"
 
 # ---------------- Models (Eventos e Workshops separados) ----------------
 class Registration(db.Model):
@@ -1123,3 +1123,4 @@ def admin_editar_workshop(workshop_id):
 # ---------------- Exec ----------------
 if __name__ == '__main__':
     app.run(debug=True)
+
